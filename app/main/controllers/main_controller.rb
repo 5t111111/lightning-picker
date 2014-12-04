52cf4ab0d5d3debdfff19ce7aed0f4b550cd8be0
WAKASUGI MUBAE <baenej@gmail.com>
2014-12-04 23:15:44 +0900

    Implement almost all features

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -8,6 +8,37 @@ class MainController < Volt::ModelController
     # Add code for when the about view is loaded
   end
 
+  def add_person
+    page._persons << {name: page._new_person}
+    page._new_person = ''
+  end
+
+  def remove_person(person)
+    page._persons.delete(person)
+  end
+
+  def pick_up
+    puts 'INFO: pick_up called.'
+    person = page._persons.sample
+    puts person._name
+    page._persons.delete(person)
+    page._picked_persons << { name: person._name }
+  end
+
+  def view_history
+    puts 'INFO: view_history called.'
+    page._picked_persons.each_with_index do |person, i|
+      puts "#{i + 1}: #{person._name}"
+    end
+  end
+
+  def clear_history
+    puts 'INFO: clear_history called.'
+    result = `confirm('Are you sure you want to clear history? (This action cannot be undone)');`
+    return unless result
+    page._picked_persons.clear
+  end
+
   private
 
   # The main template contains a #template binding that shows another
